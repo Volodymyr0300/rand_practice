f4aad6b40b376736656bf94fa59a9c28f6c88acd
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-04-26 13:21:11 -0500

    custom_types

--- rand_practice/src/main.rs
+++ rand_practice/src/main.rs
@@ -1,170 +1,76 @@
+// An attribute to hide warnings for unused code.
+#![allow(dead_code)]
+
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: u8,
+}
+
+// A unit struct
+struct Unit;
+
+// A tuple struct
+struct Pair(i32, f32);
+
+// A struct with two fields
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+// Structs can be reused as fields of another struct
+struct Rectangle {
+    // A rectangle can be specified by where the top left and bottom right
+    // corners are in space.
+    top_left: Point,
+    bottom_right: Point,
+}
+
 fn main() {
-    // ! Primitives
-    // Variables can be type annotated.
-    let _logical: bool = true;
+    // Create struct with field init shorthand
+    let name = String::from("Peter");
+    let age = 27;
+    let peter = Person { name, age };
 
-    let _a_float: f64 = 1.0;  // Regular annotation
-    let _an_integer   = 5i32; // Suffix annotation
+    // Print debug struct
+    println!("{:?}", peter);
 
-    // Or a default will be used.
-    let _default_float   = 3.0; // `f64`
-    let _default_integer = 7;   // `i32`
+    // Instantiate a `Point`
+    let point: Point = Point { x: 5.2, y: 0.4 };
+    let another_point: Point = Point { x: 10.3, y: 0.2 };
 
-    // A type can also be inferred from context.
-    let mut _inferred_type = 12; // Type i64 is inferred from another line.
-    _inferred_type = 4294967296i64;
+    // Access the fields of the point
+    println!("point coordinates: ({}, {})", point.x, point.y);
 
-    // A mutable variable's value can be changed.
-    let mut _mutable = 12; // Mutable `i32`
-    _mutable = 21;
+    // Make a new point by using struct update syntax to use the fields of our
+    // other one
+    let bottom_right = Point { x: 10.3, ..another_point };
 
-    // Error! The type of a variable can't be changed.
-    // mutable = true;
+    // `bottom_right.y` will be the same as `another_point.y` because we used that field
+    // from `another_point`
+    println!("second point: ({}, {})", bottom_right.x, bottom_right.y);
 
-    // Variables can be overwritten with shadowing.
-    let _mutable = true;
+    // Destructure the point using a `let` binding
+    let Point { x: left_edge, y: top_edge } = point;
 
-    /* Compound types - Array and Tuple */
+    let _rectangle = Rectangle {
+        // struct instantiation is an expression too
+        top_left: Point { x: left_edge, y: top_edge },
+        bottom_right: bottom_right,
+    };
 
-    // Array signature consists of Type T and length as [T; length].
-    let _my_array: [i32; 5] = [1, 2, 3, 4, 5];
+    // Instantiate a unit struct
+    let _unit = Unit;
 
-    // Tuple is a collection of values of different types 
-    // and is constructed using parentheses ().
-    let _my_tuple = (5u32, 1u8, true, -5.04f32);
+    // Instantiate a tuple struct
+    let pair = Pair(1, 0.1);
 
-    // ! Literals and operators
-    // Integer addition
-    println!("1 + 2 = {}", 1u32 + 2);
+    // Access the fields of a tuple struct
+    println!("pair contains {:?} and {:?}", pair.0, pair.1);
 
-    // Integer subtraction
-    println!("1 - 2 = {}", 1i32 - 2);
-    // TODO ^ Try changing `1i32` to `1u32` to see why the type is important
+    // Destructure a tuple struct
+    let Pair(integer, decimal) = pair;
 
-    // Scientific notation
-    println!("1e4 is {}, -2.5e-3 is {}", 1e4, -2.5e-3);
-
-    // Short-circuiting boolean logic
-    println!("true AND false is {}", true && false);
-    println!("false AND true is {}", false && true);
-    println!("true OR false is {}", true || false);
-    println!("false OR true is {}", false || true);
-    println!("NOT true is {}", !true);
-
-    // Bitwise operations
-    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
-    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
-    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
-    println!("1 << 5 is {}", 1u32 << 5);
-    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
-
-    // Use underscores to improve readability!
-    println!("One million is written as {}", 1_000_000u32);
-
-    // ! Tuples
-
-    fn reverse(pair: (i32, bool)) -> (bool, i32) {
-        // `let` can be used to bind the members of a tuple to variables.
-        let (int_param, bool_param) = pair;
-    
-        (bool_param, int_param)
-    }
-    
-    // The following struct is for the activity.
-    #[derive(Debug)]
-    struct _Matrix(f32, f32, f32, f32);
-
-     // A tuple with a bunch of different types.
-    let long_tuple = (1u8, 2u16, 3u32, 4u64,
-                      -1i8, -2i16, -3i32, -4i64,
-                      0.1f32, 0.2f64,
-                      'a', true);
-
-    // Values can be extracted from the tuple using tuple indexing.
-    println!("Long tuple first value: {}", long_tuple.0);
-    println!("Long tuple second value: {}", long_tuple.1);
-
-    // Tuples can be tuple members.
-    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
-
-    // Tuples are printable.
-    println!("tuple of tuples: {:?}", tuple_of_tuples);
-
-    // But long Tuples (more than 12 elements) cannot be printed.
-    //let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
-    //println!("Too long tuple: {:?}", too_long_tuple);
-    // TODO ^ Uncomment the above 2 lines to see the compiler error
-
-    let pair = (1, true);
-    println!("Pair is {:?}", pair);
-
-    println!("The reversed pair is {:?}", reverse(pair));
-
-    // To create one element tuples, the comma is required to tell them apart
-    // from a literal surrounded by parentheses.
-    println!("One element tuple: {:?}", (5u32,));
-    println!("Just an integer: {:?}", (5u32));
-
-    // Tuples can be destructured to create bindings.
-    let tuple = (1, "hello", 4.5, true);
-
-    let (a, b, c, d) = tuple;
-    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
-
-    // let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
-    // println!("{:?}", matrix);
-
-    // ! Arrays and SLices
-    fn analyze_slice(slice: &[i32]) {
-        println!("First element of the slice: {}", slice[0]);
-        println!("The slice has {} elements", slice.len());
-    }
-
-    // Fixed-size array (type signature is superfluous).
-    let xs: [i32; 5] = [1, 2, 3, 4, 5];
-
-    // All elements can be initialized to the same value.
-    let ys: [i32; 500] = [0; 500];
-
-    // Indexing starts at 0.
-    println!("First element of the array: {}", xs[0]);
-    println!("Second element of the array: {}", xs[1]);
-
-    // `len` returns the count of elements in the array.
-    println!("Number of elements in array: {}", xs.len());
-
-    // Arrays are stack allocated.
-    // println!("Array occupies {} bytes", mem::size_of_val(&xs));
-
-    // Arrays can be automatically borrowed as slices.
-    println!("Borrow the whole array as a slice.");
-    analyze_slice(&xs);
-
-    // Slices can point to a section of an array.
-    // They are of the form [starting_index..ending_index].
-    // `starting_index` is the first position in the slice.
-    // `ending_index` is one more than the last position in the slice.
-    println!("Borrow a section of the array as a slice.");
-    analyze_slice(&ys[1 .. 4]);
-
-    // Example of empty slice `&[]`:
-    let empty_array: [u32; 0] = [];
-    assert_eq!(&empty_array, &[]);
-    assert_eq!(&empty_array, &[][..]); // Same but more verbose
-
-    // Arrays can be safely accessed using `.get`, which returns an
-    // `Option`. This can be matched as shown below, or used with
-    // `.expect()` if you would like the program to exit with a nice
-    // message instead of happily continue.
-    for i in 0..xs.len() + 1 { // Oops, one element too far!
-        match xs.get(i) {
-            Some(xval) => println!("{}: {}", i, xval),
-            None => println!("Slow down! {} is too far!", i),
-        }
-    }
-
-    // Out of bound indexing on array with constant value causes compile time error.
-    //println!("{}", xs[5]);
-    // Out of bound indexing on slice causes runtime error.
-    //println!("{}", xs[..][5]);
+    println!("pair contains {:?} and {:?}", integer, decimal);
 }
