31edc5ff19f718e99ab4370e002409b7939cd84a
Volodymyr Poliakov <vladimir03002@gmail.com>
2025-09-28 18:38:58 -0500

    some practice

--- rand_practice/src/main.rs
+++ rand_practice/src/main.rs
@@ -1,76 +1,41 @@
-// An attribute to hide warnings for unused code.
-#![allow(dead_code)]
-
-#[derive(Debug)]
-struct Person {
-    name: String,
-    age: u8,
-}
-
-// A unit struct
-struct Unit;
-
-// A tuple struct
-struct Pair(i32, f32);
-
-// A struct with two fields
-struct Point {
-    x: f32,
-    y: f32,
-}
-
-// Structs can be reused as fields of another struct
-struct Rectangle {
-    // A rectangle can be specified by where the top left and bottom right
-    // corners are in space.
-    top_left: Point,
-    bottom_right: Point,
-}
-
 fn main() {
-    // Create struct with field init shorthand
-    let name = String::from("Peter");
-    let age = 27;
-    let peter = Person { name, age };
+    fn square_sum(vec: Vec<i32>) -> i32 {
+        let mut sum = 0;
+        for i in vec {
+            sum += i * i;
+        }
+        sum
+    }
+    let result = square_sum(vec![1, 2, 2]);
+    println!("Square sum: {}", result);
 
-    // Print debug struct
-    println!("{:?}", peter);
+    fn create_phone_number(numbers: &[u8]) -> String {
+        format!(
+            "({}{}{}) {}{}{}-{}{}{}{}",
+            numbers[0], numbers[1], numbers[2],
+            numbers[3], numbers[4], numbers[5],
+            numbers[6], numbers[7], numbers[8], numbers[9]
+        )
+    }
+    let result = create_phone_number(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+    println!("Phone number: {}", result);
 
-    // Instantiate a `Point`
-    let point: Point = Point { x: 5.2, y: 0.4 };
-    let another_point: Point = Point { x: 10.3, y: 0.2 };
+    fn maskify(cc: &str) -> String {
+        
+        if cc.len() <= 4 {
+            cc.to_string()
+        } else {
+        format!("{}{}", "#".repeat(cc.len()), &cc[cc.len()-4..])
+        
 
-    // Access the fields of the point
-    println!("point coordinates: ({}, {})", point.x, point.y);
-
-    // Make a new point by using struct update syntax to use the fields of our
-    // other one
-    let bottom_right = Point { x: 10.3, ..another_point };
-
-    // `bottom_right.y` will be the same as `another_point.y` because we used that field
-    // from `another_point`
-    println!("second point: ({}, {})", bottom_right.x, bottom_right.y);
-
-    // Destructure the point using a `let` binding
-    let Point { x: left_edge, y: top_edge } = point;
-
-    let _rectangle = Rectangle {
-        // struct instantiation is an expression too
-        top_left: Point { x: left_edge, y: top_edge },
-        bottom_right: bottom_right,
-    };
-
-    // Instantiate a unit struct
-    let _unit = Unit;
-
-    // Instantiate a tuple struct
-    let pair = Pair(1, 0.1);
-
-    // Access the fields of a tuple struct
-    println!("pair contains {:?} and {:?}", pair.0, pair.1);
-
-    // Destructure a tuple struct
-    let Pair(integer, decimal) = pair;
-
-    println!("pair contains {:?} and {:?}", integer, decimal);
+        
+        }
+    }
+    let result = maskify("4556364607935616");
+    println!("Masked: {}", result);
+    
 }
+
+
+
+
